3e77a0fa5bb6dae6108115ed832150b131619946
Teo <serban.teodor.u2q@student.ucv.ro>
2021-10-19 09:28:33 +0300

    4.6.4 set Car’s driver (Person)

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 // 1.set Car license number by constructor, setter or by direct read
 // 2. start Car
 //3. add Person (name, address etc) to Car. Person to be added should have a name given by constructor, setter or by direct read
-
+//4. set Car’s driver (Person)
 
 class Person{
 private:
@@ -27,7 +27,7 @@ public:
 
 class Car {
 private:
-    Person *person;
+    Person *person,*driver;
     string licenseNumber;
 public:
     void setLicenseNumber(string licenseNumber) {
@@ -55,6 +55,12 @@ public:
     Person* getPerson(){
         return this->person;
     }
+    void setDriver(Person *driver){
+        this->driver = driver;
+    }
+    Person* getDriver(){
+        return this->driver;
+    }
 };
 
 int main() {
@@ -73,4 +79,12 @@ int main() {
     Car *newCar3 = new Car("DJ10AVH",andrei);
     cout << newCar3->getLicenseNumber()<<endl;
     cout << newCar3->getPerson()->getName()<<endl;
+
+    Person *alex= new Person("Alex","etc");
+    Person *vlad= new Person("Vlad","abdc");
+    Car *newCar4= new Car("DJJ1011",alex);
+    newCar4->setDriver(vlad);//4. set Car’s driver (Person)
+    cout << newCar4->getLicenseNumber()<<endl;
+    cout << newCar4->getPerson()->getName()<<endl;
+    cout<<newCar4->getDriver()->getName()<<endl;
 }
